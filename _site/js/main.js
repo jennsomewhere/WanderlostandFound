c135c27dcf0843381800c009672ac31f465dcb98
Wanderlostandfound <36835783+Wanderlostandfound@users.noreply.github.com>
2018-03-05 22:12:46 +1100

    lots of changes

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,4 +1,12 @@
-console.log('Yes!');
+function loaded () {
 
+  // Fade out the loading div
+  $('body').addClass('loaded');
 
-$(document).ready();
+  // Hide the loading div
+  setTimeout(function(){
+    $('#loading').hide();
+  }, 1000);
+}
+
+$(window).on('load', loaded);
